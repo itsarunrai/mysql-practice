742273278b16df5a3d8778638d580a53f787342d
itsarunrai <itsarunraii@gmail.com>
2025-05-11 20:04:40 +0530

    Final Commit for Logical Operatir

--- logicalOperator.sql
+++ logicalOperator.sql
@@ -1,22 +1,31 @@
-use mylearnings;
+USE mylearnings;
 
--- Logical Operator --
+-- Logical Operators --
 
--- Using the AND Operator = Both be TRUE--
+-- Using the AND Operator: Both conditions must be TRUE --
+-- Retrieve data where age is less than 19 and ID is greater than 2 --
 
--- Retrive the data where age is < 19 and id > 2 --
+SELECT * FROM student 
+WHERE stud_age < 19 
+  AND id > 2;
 
-select * from student 
-where stud_age < 19 
-AND id > 2;
+-- Using the OR Operator: At least one condition must be TRUE --
 
--- Using the OR Operator = One be TRUE --
+SELECT * FROM student 
+WHERE stud_fee >= 45000 
+   OR stud_name = 'Arun';
 
-select * from student 
-where stud_fee >= 45000 
-OR stud_name = 'Arun';
+-- Using the NOT Operator: Excludes rows that match the condition --
 
--- Using the NOT Operator = Excludes the match row --
+SELECT * FROM student
+WHERE NOT stud_fee = 75000;
 
-select * from student
-where NOT stud_fee = 75000;
+-- Using the BETWEEN Operator: Selects values within a given range (inclusive) --
+
+SELECT * FROM student
+WHERE stud_age BETWEEN 20 AND 24;
+
+-- Using the IN Operator: Checks if a value exists in the specified list --
+
+SELECT * FROM student
+WHERE stud_location IN ('Veeraswamy Nagar');
