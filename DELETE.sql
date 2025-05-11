bcfbdbca21d06bc652a5e82133ed730a35d92ed8
itsarunrai <itsarunraii@gmail.com>
2025-05-11 19:23:00 +0530

    Did UPDATE and DELETE session today

---
+++ DELETE.sql
@@ -0,0 +1,9 @@
+USE mylearnings;
+
+-- Using DELETE and TRUNCATE Keyword --
+
+delete from teachers where id > 8;
+
+select * from teachers;
+
+-- TRUNCATE Would be much more faster than the Delete when dealing with the large dataset --
---
+++ UPDATE.sql
@@ -0,0 +1,8 @@
+use mylearnings;
+
+-- UPDATE Keyword used to update the values of the record --
+
+update teachers set Teach_age = 45 where id = 6;
+
+
+select * from teachers;
